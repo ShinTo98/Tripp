68cad7689807da5b4445ef252663bfd74cfdf54a
Cain <cainzhou96@gmail.com>
2018-02-13 14:01:40 -0800

    add chat

---
+++ js/chat.js
@@ -0,0 +1,19 @@
+'use strict';
+function chatSubmit() {
+	//console.log($(".chat-input").val()); 
+
+	// create new text
+	var newText = document.createElement('div'); 
+	newText.className = 'user-msg'; 
+	newText.innerHTML = $(".chat-input").val(); // get input
+	$('.chat-text').append(newText); 
+
+	$(".chat-text").scrollTop($(".chat-text")[0].scrollHeight); // scroll down
+	$(".chat-input").val(''); // clear input
+    return false; 
+}
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	console.log("Javascript connected!");
+})
