c1b857725c562ea1ff9d83b80d1cd445dcab26d7
Cain <cainzhou96@gmail.com>
2018-02-14 18:40:28 -0800

    fix chat again

--- js/chat.js
+++ js/chat.js
@@ -1,22 +1,22 @@
 'use strict';
 function chatSubmit() {
-	//console.log($(".chat-input").val()); 
+	var chatInput = document.getElementsByClassName('chat-input')[0];  
+	var chatText = document.getElementsByClassName('chat-text')[0];  
 
-	if ($(".chat-input").val() == '') {
+	//console.log(chatInput.value); 
+	//return false; 
+
+	if (chatInput.value == '') {
 		return false; 
 	}
 	// create new text
 	var newText = document.createElement('div'); 
 	newText.className = 'user-msg'; 
-	newText.innerHTML = $(".chat-input").val(); // get input
-	$('.chat-text').append(newText); 
+	newText.innerHTML = chatInput.value; // get input
+	chatText.append(newText); 
 
-	$(".chat-text").scrollTop($(".chat-text")[0].scrollHeight); // scroll down
-	$(".chat-input").val(''); // clear input
-    return false; 
+	chatText.scrollTop = chatText.scrollHeight; // scroll down
+	chatInput.value = ''; // clear input
+  return false; 
 }
 
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	console.log("Javascript connected!");
-})
