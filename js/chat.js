ba04d48ea6553bc21dc6c3e9f2fa98d8cf4c3744
Cain <cainzhou96@gmail.com>
2018-02-17 22:14:36 -0800

    Add button for schedule

--- js/chat.js
+++ js/chat.js
@@ -20,3 +20,45 @@ function chatSubmit() {
   return false; 
 }
 
+function showPanel() {
+	var panel = document.getElementsByClassName('add-task-panel')[0]; 
+	panel.style.display = 'block'; 
+}
+
+function addSubmit() {
+	var content = document.getElementsByClassName('add-input-content')[0];  
+	var title = document.getElementsByClassName('add-input-title')[0];  
+	var table = document.getElementsByClassName('schedule-table')[0];  
+
+	if (content.value == '' || title.value == '') {
+		alert('fill in both title and content'); 
+		return false; 
+	}
+	// create new title 
+	var newTitle = document.createElement('tr'); 
+	newTitle.innerHTML += '<td><div class="circle schedule-dot"></div></td>'; 
+	var newTD = document.createElement('td'); 
+	var newDiv = document.createElement('div'); 
+	newDiv.className = 'date-title'; 
+	newDiv.innerHTML = title.value; 
+	newTD.appendChild(newDiv); 
+	newTitle.appendChild(newTD); 
+	table.appendChild(newTitle); 
+
+	// create new content 
+	var newContent = document.createElement('tr'); 
+	newContent.innerHTML += '<td></td>'; 
+	newTD = document.createElement('td'); 
+	newDiv = document.createElement('div'); 
+	newDiv.className = 'schedule-description'; 
+	newDiv.innerHTML = content.value; 
+	newTD.appendChild(newDiv); 
+	newContent.appendChild(newTD); 
+	table.appendChild(newContent); 
+
+	document.getElementsByClassName('add-task-panel')[0].style.display = 'none'; 
+	content.value = ''; // clear input
+	title.value = ''; // clear input
+
+  return false; 
+}
