2380e3dc8c78970fe71a685469c508e9e6b2116d
Cheng Gong <c5gong@ucsd.edu>
2018-02-26 13:23:37 -0800

    migrate preliminary SearchResult and Trips

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,12 +1,149 @@
 import React from 'react';
+import "../styles/general.css";
+import "../styles/search_result.css";
 
 class SearchResult extends React.Component {
-  render() {
-    return (
-        <div>SearchResult
-        </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            results: [
+            {
+                'name': 'Cheng',
+                'image_address': 'cheng_hangzhou.jpg',
+                'locations': [
+                    'Hangzhou',
+                    'San Diego'
+                ],
+                'description': 'Look! I can also be a tour guide here in Hangzhou. It is a truly beautiful city. I am also pretty good at casino.'
+            },
+            {
+                'name': 'Shantao',
+                'image_address': 'shantao_casino.png',
+                'locations': [
+                    'Wuhan',
+                    'San Diego'
+                ],
+                'description': 'I am great at casino!'
+            },
+            {
+                'name': 'Chao',
+                'image_address': 'chao_great.jpeg',
+                'locations': [
+                    'Hangzhou',
+                    'San Diego'
+                ],
+                'description': 'I am cool and I will be a great tour guide for you!'
+            },
+            {
+                'name': 'Sherlock',
+                'image_address': 'sherlock.jpg',
+                'locations': [
+                    'Baker Street'
+                ],
+                'description': 'Anderson, don\'t talk out loud. You lower the IQ of the whole street.'
+            },
+            {
+                'name': 'John',
+                'image_address': 'john.jpeg',
+                'locations': [
+                    'Baker Street'
+                ],
+                'description': 'Alright, alright. Girls. Calm down.'
+            },
+            {
+                'name': 'Moriarty',
+                'image_address': 'moriarty.jpg',
+                'locations': [
+                    'Baker Street'
+                ],
+                'description': 'That\'s what people do!'
+            },
+            {
+                'name': 'Eurus',
+                'image_address': 'eurus.jpg',
+                'locations': [
+                    'Baker Street'
+                ],
+                'description': 'I\'m Eurus. Silly name, isn\'t it? Greek. Means “the east wind”. My parents loved silly names, like Eurus… or Mycroft… or Sherlock. Oh, look at him. Didn\'t it ever occur to you, not even once, that Sherlock\'s secret brother might just be Sherlock\'s secret sister? Huh? He\'s making a funny face. I think I\'ll put a hole in it.'
+            }]
+        };
+    }
+    
+    componentDidMount() {
+        this.loadResults();
+    }
+    
+    loadResults() {
+
+        // parse user search params
+        let urlParams = new URLSearchParams(window.location.search);
+        let destination = urlParams.get('destination');
+        let start_time = urlParams.get('start-time');
+        let end_time = urlParams.get('end-time');
+    
+        let result_container = document.getElementsByClassName('result-container')[0];
+        let result_found = false;
+    
+        // display the user searched filter
+        let result_info = document.createElement('div');
+        result_info.innerHTML = "Your trip to " + destination + ", " + start_time
+            + " to " + end_time + ": here are the available tour guides for you:";
+        result_container.appendChild(result_info);
+    
+        // for each of the returned results of trip tour guides, determine to put
+        // into html
+        this.state.results.forEach( (tour_guide) => {
+    
+            // filter tour_guides with destination: if the tour guide does not tour
+            // this location, skip
+            if (!tour_guide['locations'].includes(destination)) return;
+            result_found = true;
+            
+            // display tour guide
+            let result_div = document.createElement('div');
+            result_div.className = 'result';
+    
+            let image = document.createElement('img');
+            image.setAttribute('src', '../image/searchresult/' + tour_guide['image_address']);
+            image.setAttribute('alt', 'image here');
+    
+            let name = document.createElement('p');
+            name.setAttribute('class', 'result-name');
+            name.innerHTML = tour_guide['name'];
+            
+            let description = document.createElement('p');
+            description.setAttribute('class', 'result-description');
+            description.innerHTML = tour_guide['description'];
+            
+            let a = document.createElement('a');
+            a.setAttribute('class', 'nav-text pic-text a-link-style button-color');
+            a.setAttribute('href', '/progress.html?other=' + tour_guide['name'] + '&as=' + 'tourist');
+            a.innerHTML = 'Connect';
+    
+            result_div.appendChild(image);
+            result_div.appendChild(name);
+            result_div.appendChild(description);
+            result_div.appendChild(a);
+    
+            result_container.appendChild(result_div);
+        });
+    
+        // modify the info if the search at that destination did not yield any results
+        if (!result_found) {
+        result_info.innerHTML = "Your trip to " + destination + ", " + start_time
+            + " to " + end_time + ": did not return any results";
+        }
+    
+    }
+
+    render() {
+        return (
+            <div id="main-frame">
+                <div className="result-container"></div>
+            </div>
+        );
+    }
 }
 
 export default SearchResult;
--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,10 +1,139 @@
 import React from 'react';
+import "../styles/general.css";
+import "../styles/trips.css";
 
 class Trips extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      trips: [
+        {
+            'name': 'Suzuka',
+            'image_address': 'image/empty_profile_pic.png',
+            'date': 'Date: 9/1/2018 - 9/8/2018',
+            'action': '&gt;',
+            'progress': 'Progress 2/6: Action Needed! Discuss',
+            'own_identity': 'tourist'
+        },
+        {
+            'name': 'Kato',
+            'image_address': 'image/empty_profile_pic.png',
+            'date': 'Date: 10/3/2018 - 10/7/2018',
+            'action': '&gt;',
+            'progress': 'Progress 1/6: Action Needed! Accept/Reject',
+            'own_identity': 'tour guide'
+        },
+        {
+            'name': 'Shantao',
+            'image_address': 'image/empty_profile_pic.png',
+            'date': 'Date: 4/9/2018 - 4/20/2018',
+            'action': '&lt;',
+            'progress': 'Progress 3/6: Waiting for Resceduling',
+            'own_identity': 'tourist'
+        },
+        {
+            'name': 'Chao',
+            'image_address': 'image/empty_profile_pic.png',
+            'date': 'Date: 8/1/2018 - 8/8/2018',
+            'action': '&lt;',
+            'progress': 'Progress 1/6: Waiting to hear back',
+            'own_identity': 'tourist'
+        }
+      ]
+    };
+  }
+  componentDidMount() {
+    this.loadTrips();
+  }
+
+  // load results of trip tour guides at the given location
+  loadTrips() {
+
+    // parse user search params
+    let urlParams = new URLSearchParams(window.location.search);
+    // let ? = urlParams.get('');
+
+    let trip_container = document.getElementsByClassName('trip-container')[0];
+
+    this.state.trips.forEach( (trip) => {
+
+      // display trip
+      let trip_div = document.createElement('div');
+      trip_div.className = 'trip';
+
+      let image = document.createElement('img');
+      image.setAttribute('src', trip['image_address']);
+      image.setAttribute('alt', 'image here');
+
+      let name = document.createElement('div');
+      name.className = 'trip-others-name';
+      name.innerHTML = trip['name'];
+
+      let date = document.createElement('div');
+      date.className = 'trip-date';
+      date.innerHTML = trip['date'];
+
+      let action = document.createElement('div');
+      action.className = 'trip-action-needed';
+      action.innerHTML = trip['action'];
+      
+      let progress = document.createElement('div');
+      progress.className = 'trip-progress';
+      progress.innerHTML = trip['progress'];
+
+      let schedule_link = document.createElement('a');
+      schedule_link.className = 'nav-text a-link-style';
+      schedule_link.setAttribute('href', '/schedule.html?as=' + trip['own_identity']
+          + '&other=' + trip['name']);
+      schedule_link.innerHTML = 'Schedule';
+
+      let progress_link = document.createElement('a');
+      progress_link.className = 'nav-text a-link-style';
+      progress_link.setAttribute('href', '/progress.html?as=' + trip['own_identity']
+          + '&other=' + trip['name']);
+      progress_link.innerHTML = 'Progress';
+      
+      let delete_link = document.createElement('button');
+      delete_link.className = 'nav-text a-link-style';
+      delete_link.setAttribute('onclick', 'deleteTrip(' + '"' + trip['name'] + '"' + ')');
+      delete_link.innerHTML = 'delete';
+
+      trip_div.appendChild(image);
+      trip_div.appendChild(name);
+      trip_div.appendChild(date);
+      trip_div.appendChild(action);
+      trip_div.appendChild(progress);
+      trip_div.appendChild(schedule_link);
+      trip_div.appendChild(progress_link);
+      trip_div.appendChild(delete_link);
+      
+      trip_container.appendChild(trip_div);
+    });
+  }
+
+  removeTrips() {
+    let trip_container = document.getElementsByClassName('trip-container')[0];
+    while (trip_container.firstChild) {
+        trip_container.removeChild(trip_container.firstChild);
+    }
+  }
+
+  deleteTrip(name) {
+    this.state.trips.forEach( (part, index) => {
+        if (part['name'] !== name) return;
+        this.state.trips.splice(index, 1);
+    });
+
+    this.removeTrips();
+    this.loadTrips();
+  }
+
   render() {
     return (
-        <div>Trips
-        </div>
+      <div id="main-frame">
+        <div className="trip-container"></div>
+      </div>
     );
   }
 }
