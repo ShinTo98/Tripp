0c8048ce2817c5c2ce64ea89c2641207eeae4811
Shantao Yi <shantao.yi@gmail.com>
2018-03-02 21:05:04 -0800

    minor naming change

---
+++ src/components/auth/GuideSignup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class GuideSignup extends React.Component {
+  render() {
+    return (
+        <div>GuideSignup</div>
+    );
+  }
+}
+
+export default GuideSignup;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import MainWithHeader from './components/MainWithHeader';
-import WelcomePage from './components/WelcomePage';
 
+import WelcomePage from './components/WelcomePage';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
-import GuideSignupInfo from './components/GuideSignupInfo';
+import GuideSignup from './components/auth/GuideSignup';
 import Profile from './components/Profile';
 import Progress from './components/Progress';
 import Schedule from './components/Schedule';
@@ -15,15 +14,12 @@ import Trips from './components/Trips';
 export default (
   <div>
     <Route path="/" component={WelcomePage} />
-    <Route path="/login" component={MainWithHeader}>
-      <IndexRoute component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/guideSignupInfo" component={GuideSignupInfo} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/progress" component={Progress} />
-      <Route path="/schedule" component={Schedule} />
-      <Route path="/searchResult" component={SearchResult} />
-      <Route path="/trips" component={Trips} />
-    </Route>
+    <Route path="/signup" component={Signup} />
+    <Route path="/guideSignupInfo" component={GuideSignup} />
+    <Route path="/profile" component={Profile} />
+    <Route path="/progress" component={Progress} />
+    <Route path="/schedule" component={Schedule} />
+    <Route path="/searchResult" component={SearchResult} />
+    <Route path="/trips" component={Trips} />
   </div>
 );
