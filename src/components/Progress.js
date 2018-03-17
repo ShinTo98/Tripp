d08cdb0c18ced716f38421a06e09c97ab52e72e7
Cain <cainzhou96@gmail.com>
2018-03-17 13:28:29 -0700

    css module for progress

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,50 +1,110 @@
-import React, {PropTypes} from 'react';
-import {connect} from 'react-redux'; 
-import {bindActionCreators} from 'redux';
-import * as progressActions from '../actions/progressActions';
+import React from 'react';
+import style from '../styles/progress.module.css';
 import '../styles/progress.css';
 import Chat from "./common/Chat";
+import classNames from 'classnames'; 
 
 class Progress extends React.Component {
   constructor(props) {
     super(props); 
+    this.state = {
+      todoRight: [
+        {
+          title: 'todo1', 
+          content: '[content]', 
+          color: 'green', 
+          count: 0
+        }, 
+        {
+          title: 'todo2', 
+          content: '[content]', 
+          color: 'green', 
+          count: 1
+
+        }, 
+        {
+          title: 'todo3', 
+          content: '[content]', 
+          color: 'red', 
+          count: 2
+
+        }, 
+        {
+          title: 'todo4', 
+          content: '[content]', 
+          color: 'red', 
+          count: 3
+
+        }
+      ], 
+      todoLeft: [
+        {
+          title: 'todo1', 
+          content: '[content]', 
+          color: 'green', 
+          count: 0
+
+        }, 
+        {
+          title: 'todo2', 
+          content: '[content]', 
+          color: 'green', 
+          count: 1
+
+        }, 
+        {
+          title: 'todo3', 
+          content: '[content]', 
+          color: 'red', 
+          count: 2
+
+        }, 
+        {
+          title: 'todo4', 
+          content: '[content]', 
+          color: 'red', 
+          count: 3
+
+        }
+      ] 
+    }; 
   }
 
   render() {
     return (
         <div id="main-frame">
-          <div className="schedule-left">
-            <div className="schedule-title"><span className="other-name">Shantao</span>'s</div>
-            <div className="schedule-line"></div>
-            <table className="schedule-table">
-                {this.props.todoLeft.map(item => 
+          <div className={style.scheduleleft}>
+            <div className={style.scheduletitle}><span className={style.othername}>Shantao</span>'s</div>
+            <div className={style.scheduleline}></div>
+            <table className={style.scheduletable}>
+                {this.state.todoLeft.map(item => 
                       <tbody key={`left${item.count}`}>
                         <tr>
                           <td><div className={`circle-${item.color} schedule-dot`}></div></td>
-                          <td><div className="date-title">{item.title}</div></td>
+                          <td><div className={style.datetitle}>{item.title}</div></td>
                         </tr>
                         <tr>
                           <td></td><td><div
-                          className="schedule-description">{item.content}</div></td>
+                          className={style.scheduledescription}>{item.content}</div></td>
                         </tr>
                       </tbody>
                 )}
             </table>
             </div>
 
-          <div className="schedule-right">
-            <div className="schedule-title">Yours</div>
-            <div className="schedule-line"></div>
-            <table className="schedule-table">
-                {this.props.todoRight.map(item => 
+          <div className={style.scheduleright}>
+            <div className={style.scheduletitle}>Yours</div>
+            <div className={style.scheduleline}></div>
+            <table className={style.scheduletable}>
+                {this.state.todoRight.map(item => 
                       <tbody key={`right${item.count}`}>
                         <tr>
                           <td><div className={`circle-${item.color} schedule-dot`}></div></td>
-                          <td><div className="date-title">{item.title}</div></td>
+                          <td><div className={style.datetitle}>{item.title}</div></td>
                         </tr>
                         <tr>
                           <td></td><td><div
-                          className="schedule-description">{item.content}</div></td>
+                          className={style.scheduledescription}>{item.content}</div></td>
                         </tr>
                       </tbody>
                 )}
@@ -56,24 +116,4 @@ class Progress extends React.Component {
   }
 } 
 
-Progress.propTypes = {
-  todoLeft: PropTypes.array.isRequired,
-  todoRight: PropTypes.array.isRequired, 
-  actions: PropTypes.object.isRequired	
-};
-
-function mapStateToProps(state, ownProps) {
-  //console.log(state); 
-  return {
-		todoLeft: state.progress.todoLeft, 
-		todoRight: state.progress.todoRight 
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(progressActions, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Progress);
+export default Progress;
